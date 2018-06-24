c481cdb987887d139b274e86ab550eb4d7a81c69
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2018-06-24 13:41:47 +0530

    some 2018 updates

--- CourseTimings/schedule/src/schedule/Aug2018.scala
+++ CourseTimings/schedule/src/schedule/Aug2018.scala
@@ -11,15 +11,15 @@ import schedule.{TimingPreference => TP}
 object Aug2018 {
   implicit val aug2018: List[Course] =
     List(
-      (200, "Multivariable Calculus", gudi),
-      (212, "Algebra I", abhishek),
+      (200, "Multivariable Calculus", kverma),
+      (212, "Algebra I", pooja),
       (213, "Representation Theory", pooja),
-      (219, "Linear Algebra", somu),
-      (221, "Analysis I", khare),
+      (219, "Linear Algebra", rvenkat),
+      (221, "Analysis I", bharali),
       (223, "Functional Analysis", tirtha),
       (231, "Topology", harish),
       (242, "PDE", veluma),
-      (261, "Probablity Models", manju),
+      (261, "Probablity Models", gadgil),
       (232, "Intro to Algebraic Topology", dattab),
       (338, "Differentiable Manifolds and Lie Groups", vamsi),
       (361, "Probability Theory", skiyer),
@@ -28,24 +28,7 @@ object Aug2018 {
 
   val prefs =
     List(
-      TP(somu, t1100, m11, m12),
-      TP(skiyer, t1100, m11, m12),
-      TP(vamsi, m2, m11, m3),
-      TP(subhojoy, t330, t1100, m3),
-      TP(harish, t1100, m12, m11),
-      TP(veluma, m10, m11, m12),
-      TP(tirtha, m10, m11, m12),
-      TP.sets(khare,
-              1 -> List(t1100, t200),
-              2 -> List(t330, t930),
-              3 -> List(m2, m3, m11)),
-      TP.sets(gudi,
-              1 -> List(t1100),
-              2 -> List(t330),
-              3 -> List(m9, m10),
-              4 -> List(t930)),
-      TP.sets(pooja, 1 -> List(t200, t330), 2 -> List(m2, m3)),
-      TP(abhishek, t330, t200, m3)
+      TP(dattab, m11, m10, m12)
     )
 
   val core1: Set[Course] = Set(212, 219, 221, 231, 261) map (get)
